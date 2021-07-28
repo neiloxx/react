525e1ffd725e346511d8b94c536a3a106cc5d81b
neiloxx <anastasia.v.sido@gmail.com>
2021-07-28 19:16:03 +0600

    feat: add base webpack.config.js

--- react-components/webpack.config.js
+++ react-components/webpack.config.js
@@ -1,38 +1,84 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 const devServer = isDev =>
-  !isDev ? {} : {
-    devServer: {
-      open: true,
-      hot: true,
-      port: 8080,
-      contentBase: path.join(__dirname, 'public'),
-    }
-  }
+  !isDev
+    ? {}
+    : {
+        devServer: {
+          open: true,
+          hot: true,
+          port: 8080,
+          contentBase: path.join(__dirname, 'public'),
+        },
+      };
 
-module.exports = (develop) => ({
+const esLintPlugin = isDev =>
+  isDev ? [] : [new ESLintPlugin({ extensions: ['ts', 'tsx', 'js', 'jsx'] })];
+
+module.exports = develop => ({
   mode: develop ? 'development' : 'production',
-  entry: "./src/index.js",
+  // devtool: develop ? 'inline-source-map' : false,
+  entry: './src/index.tsx',
   output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
   },
   module: {
-    rules:[
+    rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.([jt]s|[jt]sx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        }
+        use: ['babel-loader', 'ts-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                includePaths: [path.resolve(__dirname, './src')],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
   plugins: [
+    new CopyPlugin({
+      patterns: [{ from: './public' }],
+    }),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    ...esLintPlugin(develop),
   ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
   ...devServer(develop),
-})
+});
