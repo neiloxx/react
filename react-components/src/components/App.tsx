e63115183242105b7e0c3a93727a7b1bc00c77fa
neiloxx <anastasia.v.sido@gmail.com>
2021-08-12 13:02:24 +0600

    feat: add main

--- react-components/src/components/App.tsx
+++ react-components/src/components/App.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from 'react';
 import Header from './Header/Header';
+import Main from './Main/Main';
 
 const App = (): ReactElement => {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 };
---
+++ react-components/src/components/Main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.scss';
+
+const Main: React.FC = () => {
+  return (
+    <main className="main">
+      <p>some text</p>
+    </main>
+  );
+};
+
+export default Main;
