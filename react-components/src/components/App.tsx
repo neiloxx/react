e716a69952c212bd270e722a9cc78fe0d0757ef4
neiloxx <anastasia.v.sido@gmail.com>
2021-08-12 12:52:16 +0600

    feat: add header

--- react-components/src/components/App.tsx
+++ react-components/src/components/App.tsx
@@ -1,5 +1,12 @@
 import React, { ReactElement } from 'react';
+import Header from './Header/Header';
 
-const App = (): ReactElement => <div>Hello, World!</div>;
+const App = (): ReactElement => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
 
 export default App;
