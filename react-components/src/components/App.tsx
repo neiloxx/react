a75aa40e28094eb1a407a43252340b10cef0a4f7
neiloxx <anastasia.v.sido@gmail.com>
2021-08-12 13:03:29 +0600

    feat: add footer

--- react-components/src/components/App.tsx
+++ react-components/src/components/App.tsx
@@ -1,12 +1,14 @@
 import React, { ReactElement } from 'react';
 import Header from './Header/Header';
 import Main from './Main/Main';
+import Footer from './Footer/Footer';
 
 const App = (): ReactElement => {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 };
---
+++ react-components/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <a className="footer__link" href="https://github.com/neiloxx">
+        github
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
