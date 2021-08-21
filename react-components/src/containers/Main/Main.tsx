43417fcc8e989227dcb34c3ff9278267ccf34360
neiloxx <anastasia.v.sido@gmail.com>
2021-08-21 15:46:19 +0600

    feat: add Card component and implement it

--- react-components/src/containers/Main/Main.tsx
+++ react-components/src/containers/Main/Main.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import cards from '../../collections/cards';
+import Card from '../../components/Card/Card';
 
 import './style.scss';
 
@@ -6,6 +8,15 @@ const Main: React.FC = () => {
   return (
     <main className="main">
       <h2 className="main__title">some text</h2>
+      {cards.map(card => (
+        <Card
+          name={card.name}
+          image={card.image}
+          url={card.url}
+          cities={card.cities}
+          key={card.name}
+        />
+      ))}
     </main>
   );
 };
