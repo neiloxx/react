66cc15d414fcf6887c56e25ee869f37a2689ae01
neiloxx <anastasia.v.sido@gmail.com>
2021-08-24 16:50:46 +0600

    feat: update api

--- react-api/src/api/api.ts
+++ react-api/src/api/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+export const API_KEY = 'a9aa9685217441b789b30ff7911f73a0';
+
 const axiosInstance = axios.create({
   baseURL: 'https://newsapi.org/',
   timeout: 5000,
