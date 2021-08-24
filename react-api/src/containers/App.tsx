c481727c45f1c686af5daf42d4b9c7f14b1a6cc9
neiloxx <anastasia.v.sido@gmail.com>
2021-08-24 14:14:43 +0600

    feat: add configurations webpack, eslint, prettier and project's base structure

---
+++ react-api/src/containers/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const App: React.FC = () => {
+  return (
+    <main className="main">
+      <h1 className="main__title">react. api</h1>
+    </main>
+  );
+};
+
+export default App;
