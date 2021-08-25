8152ffbff6ba5f166d77f71b9d4c82a18da2553c
neiloxx <anastasia.v.sido@gmail.com>
2021-08-25 11:15:50 +0600

    feat: add sort search

--- react-api/src/components/SearchForm/SearchForm.tsx
+++ react-api/src/components/SearchForm/SearchForm.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, useState } from 'react';
 import { AxiosResponse } from 'axios';
 import axiosInstance, { API_KEY } from '../../api/api';
+import { SortType } from '../../types';
 
 import './style.scss';
 
@@ -11,6 +12,7 @@ interface IForm {
 const SearchForm: React.FC<IForm> = ({ setArticles }) => {
   const [searchValue, setSearchValue] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [sortBy, setSortBy] = useState<SortType>(SortType.popularity);
 
   const handleSubmit = async (
     e: ChangeEvent<HTMLFormElement>,
@@ -19,7 +21,7 @@ const SearchForm: React.FC<IForm> = ({ setArticles }) => {
     setIsLoading(true);
     try {
       const response: AxiosResponse = await axiosInstance.get(
-        `v2/everything?q=${searchValue}&apiKey=${API_KEY}`,
+        `v2/everything?q=${searchValue}&apiKey=${API_KEY}&sortBy=${sortBy}&pageSize=8`,
       );
       setArticles(response.data.articles);
     } catch (err: any) {
@@ -31,21 +33,58 @@ const SearchForm: React.FC<IForm> = ({ setArticles }) => {
 
   return (
     <form className="search-form" onSubmit={handleSubmit}>
-      <label className="search-bar" htmlFor="search-bar">
-        <input
-          className="search-bar__input"
-          id="search-bar"
-          type="text"
-          value={searchValue}
-          placeholder="Search..."
-          onChange={e => setSearchValue(e.target.value)}
-        />
+      <div className="search-form__search-bar">
+        <label className="search-form__search-bar__label" htmlFor="search-bar">
+          <input
+            className="search-form__search-bar__input"
+            id="search-bar"
+            type="text"
+            value={searchValue}
+            placeholder="Search..."
+            onChange={e => setSearchValue(e.target.value)}
+          />
+        </label>
         <button
-          className="search-bar__btn"
+          className="search-form__search-bar__btn"
           type="submit"
           disabled={!searchValue || isLoading}
         />
-      </label>
+      </div>
+      <div className="search-form__radios">
+        <label className="search-form__radios-label" htmlFor="radio-relevancy">
+          <input
+            className="search-form__radios-input"
+            id="radio-relevancy"
+            type="radio"
+            value={SortType.relevancy}
+            checked={sortBy === SortType.relevancy}
+            onChange={() => setSortBy(SortType.relevancy)}
+          />
+          relevancy
+        </label>
+        <label className="search-form__radios-label" htmlFor="radio-popularity">
+          <input
+            className="search-form__radios-input"
+            id="radio-popularity"
+            type="radio"
+            value={SortType.popularity}
+            checked={sortBy === SortType.popularity}
+            onChange={() => setSortBy(SortType.popularity)}
+          />
+          popularity
+        </label>
+        <label className="search-form__radios-label" htmlFor="radio-published">
+          <input
+            className="search-form__radios-input"
+            id="radio-published"
+            type="radio"
+            value={SortType.publishedAt}
+            checked={sortBy === SortType.publishedAt}
+            onChange={() => setSortBy(SortType.publishedAt)}
+          />
+          published
+        </label>
+      </div>
     </form>
   );
 };
