d41d54a3d7c4b3e18a3b84ac79fb018e2f05240e
neiloxx <anastasia.v.sido@gmail.com>
2021-08-25 10:53:13 +0600

    feat: update styles and connect components to the main page

--- react-api/src/containers/App.tsx
+++ react-api/src/containers/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SearchForm from '../components/SearchForm/SearchForm';
+import Article from '../components/Article/Article';
 import './style.scss';
 
+const defaultImage = './public/assets/images/default-image.jpg';
+
 const App: React.FC = () => {
+  const [articles, setArticles] = useState<Array<any>>([]);
+
   return (
     <main className="main">
       <h1 className="main__title">react. api</h1>
+      <SearchForm setArticles={setArticles} />
+      <div className="main__articles-field">
+        {articles.map(article => (
+          <Article
+            key={`${article.author}${article.title}`}
+            author={article.author}
+            title={article.title}
+            content={article.content}
+            publishedAt={article.publishedAt}
+            source={article.source}
+            urlToImage={article.urlToImage || defaultImage}
+          />
+        ))}
+      </div>
     </main>
   );
 };
