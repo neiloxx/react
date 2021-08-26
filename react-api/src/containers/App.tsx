14e38d899f326445ccd62c7067192ee0d0b86d5e
neiloxx <anastasia.v.sido@gmail.com>
2021-08-26 12:26:01 +0600

    feat: add loading&no-result condition

--- react-api/src/containers/App.tsx
+++ react-api/src/containers/App.tsx
@@ -7,23 +7,32 @@ const defaultImage = './public/assets/images/default-image.jpg';
 
 const App: React.FC = () => {
   const [articles, setArticles] = useState<Array<any>>([]);
+  const [results, setResults] = useState<number>(0);
 
   return (
     <main className="main">
       <h1 className="main__title">react. api</h1>
-      <SearchForm setArticles={setArticles} />
+      <SearchForm setArticles={setArticles} setResults={setResults} />
       <div className="main__articles-field">
-        {articles.map(article => (
-          <Article
-            key={`${article.author}${article.title}`}
-            author={article.author}
-            title={article.title}
-            content={article.content}
-            publishedAt={article.publishedAt}
-            source={article.source}
-            urlToImage={article.urlToImage || defaultImage}
-          />
-        ))}
+        {!articles.length ? (
+          results ? (
+            <p>Loading</p>
+          ) : (
+            <p>No results</p>
+          )
+        ) : (
+          articles.map(article => (
+            <Article
+              key={`${article.author}-${article.title}-${article.publishedAt}`}
+              author={article.author}
+              title={article.title}
+              content={article.content}
+              publishedAt={article.publishedAt}
+              source={article.source}
+              urlToImage={article.urlToImage || defaultImage}
+            />
+          ))
+        )}
       </div>
     </main>
   );
