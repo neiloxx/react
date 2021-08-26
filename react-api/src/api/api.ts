2b2fba4d8036c9c61352f69f61cbe7c7db476e82
neiloxx <anastasia.v.sido@gmail.com>
2021-08-26 12:23:38 +0600

    refactor: replace function for connection to the api

--- react-api/src/api/api.ts
+++ react-api/src/api/api.ts
@@ -1,10 +1,22 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import { SortType } from '../types';
 
-export const API_KEY = 'a9aa9685217441b789b30ff7911f73a0';
+const API_KEY = 'e8312a66378248f4aaa1299a34a2d6be';
+export const MAX_SERVER_RESULT = 100;
 
 const axiosInstance = axios.create({
   baseURL: 'https://newsapi.org/',
   timeout: 5000,
 });
+
+export const getResponse = async (
+  searchValue: string,
+  sortBy: SortType,
+  artOnPage: number,
+  page: number,
+): Promise<AxiosResponse> =>
+  axiosInstance.get(
+    `v2/everything?q=${searchValue}&apiKey=${API_KEY}&sortBy=${sortBy}&pageSize=${artOnPage}&page=${page}`,
+  );
 
 export default axiosInstance;
